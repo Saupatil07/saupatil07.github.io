9cb97dd4456d6516ce514d2c8c9e8ba7f1364df5
Saurabh patil <sp3667656@gmail.com>
2026-03-10 16:40:44 +0530

    Add Intersection Observer for fade-in elements

---
+++ script.js
@@ -0,0 +1,18 @@
+const observer = new IntersectionObserver(entries => {
+
+entries.forEach(entry => {
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("visible");
+
+}
+
+});
+
+});
+
+
+const hiddenElements = document.querySelectorAll(".fade-in");
+
+hiddenElements.forEach(el => observer.observe(el));
